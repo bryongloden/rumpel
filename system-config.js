5b06893599a83187d6521df6b6dbb36d46de83d2
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2016-06-30 11:30:08 +0100

    Deploy

---
+++ system-config.js
@@ -0,0 +1,62 @@
+// SystemJS configuration file, see links for more information
+// https://github.com/systemjs/systemjs
+// https://github.com/systemjs/systemjs/blob/master/docs/config-api.md
+/***********************************************************************************************
+ * User Configuration.
+ **********************************************************************************************/
+/** Map relative paths to URLs. */
+var map = {
+    'moment': 'vendor/moment',
+    'primeng': 'vendor/primeng',
+    'angular2-jwt': 'vendor/angular2-jwt'
+};
+/** User packages configuration. */
+var packages = {
+    'moment': { format: 'cjs', main: 'moment.js' },
+    'primeng': { defaultExtension: 'js' },
+    'angular2-jwt': { main: 'angular2-jwt.js' }
+};
+////////////////////////////////////////////////////////////////////////////////////////////////
+/***********************************************************************************************
+ * Everything underneath this line is managed by the CLI.
+ **********************************************************************************************/
+var barrels = [
+    // Angular specific barrels.
+    '@angular/core',
+    '@angular/common',
+    '@angular/compiler',
+    '@angular/http',
+    '@angular/router',
+    '@angular/platform-browser',
+    '@angular/platform-browser-dynamic',
+    // Thirdparty barrels.
+    'rxjs',
+    // App specific barrels.
+    'app',
+    'app/shared',
+    'app/rump-header',
+    'app/rump-footer',
+    'app/rump-side-menu',
+    'app/dashboard',
+    'app/dataViews',
+    'app/services',
+    'app/user-mgmt',
+    'app/pipes',
+    'app/transactions',
+];
+var cliSystemConfigPackages = {};
+barrels.forEach(function (barrelName) {
+    cliSystemConfigPackages[barrelName] = { main: 'index' };
+});
+// Apply the CLI SystemJS configuration.
+System.config({
+    map: {
+        '@angular': 'vendor/@angular',
+        'rxjs': 'vendor/rxjs',
+        'main': 'main.js'
+    },
+    packages: cliSystemConfigPackages
+});
+// Apply the user's configuration.
+System.config({ map: map, packages: packages });
+//# sourceMappingURL=system-config.js.map
